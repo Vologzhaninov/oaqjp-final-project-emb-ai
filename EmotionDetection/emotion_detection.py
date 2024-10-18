75be24f1ed15621ab13259e54fee2f6b71ad1f96
Vologzhaninov <d.vologzhaninov@gmail.com>
2024-10-18 00:42:39 -0400

    First changes

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    urlres = 'https://sn-watson-emotion.labs.skills.network'
+    url = urlres + '/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(url, json=myobj, headers=header, timeout=60)
+    response_json = json.loads(response.text)
+    emotions_dic = response_json['emotionPredictions'][0]['emotion']
+    anger_score = emotions_dic['anger']
+    disgust_score = emotions_dic['disgust']
+    fear_score = emotions_dic['fear']
+    joy_score = emotions_dic['joy']
+    sadness_score = emotions_dic['sadness']
+    max_score = max(emotions_dic.values())
+    key_list = list(emotions_dic.keys())
+    val_list = list(emotions_dic.values())
+    dominant_emotion = key_list[val_list.index(max_score)]
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def get_emotion_detection():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    text = "For the given statement, the system response is"
+    text = text + " 'anger': " + str(response['anger'])
+    text = text + ", 'disgust': " + str(response['disgust'])
+    text = text + ", 'fear': " + str(response['fear'])
+    text = text + ", 'joy': " + str(response['joy'])
+    text = text + " and 'sadness': " + str(response['sadness'])
+    text = text + ". The dominant emotion is " + response['dominant_emotion'] + "."
+    return text
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
